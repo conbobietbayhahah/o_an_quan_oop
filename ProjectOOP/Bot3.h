1d70dcc07bc489caefd749681940592da21365d8
Phuc2007 <vohoangphuc2021vl@gmail.com>
2026-05-08 01:48:53 +0700

    Update Bot3.h

--- ProjectOOP/Bot3.h
+++ ProjectOOP/Bot3.h
@@ -1,1 +1,11 @@
 #pragma once
+#include "Player.h"
+#include "Board.h"
+#include <iostream>
+using namespace std;
+
+class Bot3 : public Player {
+public:
+    Bot3(string n);
+    int makeMove(Board &board) override;
+};
