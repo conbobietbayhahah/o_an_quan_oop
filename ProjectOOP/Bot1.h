67124b404cd64bda69b8e3ff27d97a7b36f3ded5
Phuc2007 <vohoangphuc2021vl@gmail.com>
2026-05-08 01:46:46 +0700

    Add Bot1 class with move functionality

--- ProjectOOP/Bot1.h
+++ ProjectOOP/Bot1.h
@@ -1,1 +1,13 @@
 #pragma once
+#include "Player.h"
+#include "Board.h"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+class Bot1 : public Player {
+public:
+    Bot1(string n);
+    int makeMove(Board &board) override;
+};
