d24fcc39028b6e89f6e87b4bf05aba4b17b42f69
Phuc2007 <vohoangphuc2021vl@gmail.com>
2026-05-12 19:53:36 +0700

    Update Game.h

--- ProjectOOP/Game.h
+++ ProjectOOP/Game.h
@@ -2,38 +2,53 @@
 #define GAME_H
 
 #include <vector>
+
 #include "Board.h"
 
 class OAnQuan {
 private:
     Board banCo;
-    std::vector<int> captured;        
-    int currentPlayer;                
-    int quanValue;                    
-    int soLuotDaChoi;                 
-    bool isFirstTurn;                 
-    static const int MAX_LUOT = 200;  
+
+    std::vector<int> captured;
+
+    int currentPlayer;
+    int quanValue;
+    int soLuotDaChoi;
+
+    bool isFirstTurn;
+
+    static const int MAX_LUOT = 200;
 
 public:
     OAnQuan();
-    
-    bool diChuyen(int oBatDau, bool thuancChieu);
-    void raiQuan(int pos, bool thuancChieu);
-    void anQuan(int pos, bool thuancChieu);
+
+    bool diChuyen(int oBatDau, bool thuanChieu);
+
+    void raiQuan(int pos, bool thuanChieu);
+
+    void anQuan(int pos, bool thuanChieu);
+
     void vayDanKhiHetQuan();
-    
+
     int tinhDiem(int player) const;
+
     bool hetQuan() const;
+
     bool daHetGioiHanLuot() const;
-    bool ketThucTroChoi() const;      
+
+    bool ketThucTroChoi() const;
+
     int nguoiThang() const;
-    
-    int laySoLuotDaChoi() const { return soLuotDaChoi; }
-    int nguoiChoiluot() const { return currentPlayer; }
-    std::vector<int> danDaAn() const { return captured; }
-    
-    Board& layBanCo() { return banCo; }
-    const Board& layBanCo() const { return banCo; }
+
+    int laySoLuotDaChoi() const;
+
+    int nguoiChoiluot() const;
+
+    std::vector<int> danDaAn() const;
+
+    Board& layBanCo();
+
+    const Board& layBanCo() const;
 };
 
 #endif
